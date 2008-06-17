467a9a212dc599d051301e649b7bf27349e79af5
James Cook <mokus@deepbondi.net>
2008-06-17 11:25:39 +0000

    init: minor cleanup of GetOpts.hs

--- init/src/Util/GetOpts.hs
+++ init/src/Util/GetOpts.hs
@@ -18,7 +18,6 @@ import System.Exit
 
 import qualified Data.Map as M
 
-import Control.Arrow
 import Data.Monoid
 
 data OptSpec a 
@@ -39,22 +38,26 @@ getOpts optSpecs usage = do
 getOpts' :: [OptSpec a] -> IO b -> [String] -> IO ([a], [String])
 getOpts' optSpecs usage args = process [] [] args
         where
-                shortForms = M.fromList (map (optShortForm &&& id) optSpecs)
-                longForms  = M.fromList (map (optLongForm  &&& id) optSpecs)
+                indexBy f = map (\x -> (f x, x))
+                shortForms = M.fromList (indexBy optShortForm optSpecs)
+                longForms  = M.fromList (indexBy optLongForm  optSpecs)
                 
                 onErr code msg = do
                         putStrLn msg
                         usage
                         exitWith (ExitFailure code)
                 
+                -- process options up to end of input or first "--"
                 process opts args [] 
                         = return (reverse opts, reverse args)
                 process opts args ("--":rest)
                         = return (reverse opts, reverse args ++ rest)
                 
+                -- on "--help" print usage and exit nicely
                 process opts args ("--help":rest)
                         = usage >> exitWith ExitSuccess
                 
+                -- on "--foo" process the "foo" long option
                 process opts args (arg@('-':'-':flag):rest)
                         = case M.lookup flag longForms of
                                 Nothing -> process opts (arg:args) rest
@@ -62,11 +65,15 @@ getOpts' optSpecs usage args = process [] [] args
                                 Just (spec@(Param {}))  -> case rest of
                                         (param:rest)    -> process (paramValue spec param : opts) args rest 
                                         []              -> onErr (-1) ("Error: " ++ arg ++ " requires a parameter.")
+                
+                -- on "-bar" process the 'b', 'a' and 'r' short options
                 process opts args (arg@('-':flags):rest)
                         = processShort opts args flags rest
                 
+                -- on anything else pass the arg to the 'args' output list
                 process opts args (arg:rest) = process opts (arg:args) rest
                 
+                -- helper for processing short args
                 processShort opts args [] rest
                         = process opts args rest
                 processShort opts args (f:fs) rest
